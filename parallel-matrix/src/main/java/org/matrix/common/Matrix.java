f4702185247675a46349d4ec055fabff59165619
aimers1975 <amy_hindman@yahoo.com>
2015-07-09 05:44:39 -0500

    Moved missing functions to new Matrix/test class

--- parallel-matrix/src/main/java/org/matrix/common/Matrix.java
+++ parallel-matrix/src/main/java/org/matrix/common/Matrix.java
@@ -28,6 +28,15 @@ public class Matrix {
 		}		
 	}
 		
+	public boolean setRow(double[] inputRow, int rowID) {
+		if(inputRow.length == matrix[0].length && rowID < matrix.length) {
+		    matrix[rowID] = inputRow;
+		    return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public double getElem(int i, int j) {
 		return matrix[i][j];
 	}
@@ -40,10 +49,10 @@ public class Matrix {
 			return false;
 		}
 	}
-		
+	
 	public double[] getRow(int row) {
 		if(row < getNumRows()) 
-			return matrix[row];
+		    return matrix[row];
 		else
 			return new double[0]; // need to fix this
 	}
@@ -51,27 +60,142 @@ public class Matrix {
 	public double[] getColumn(int col) {
 		return matrix[col];
 	}
-		
+	
 	public int getNumRows() {
 		return matrix.length;
 	}
-		
+	
 	public int getNumColumns() {
 		return matrix[0].length;
-	}
-		
-	public boolean setRow(double[] inputRow, int rowID) {
-		if(inputRow.length == matrix[0].length && rowID < matrix.length) {
-			matrix[rowID] = inputRow;
-			return true;
-		} else {
-			return false;
-		}
 	}
 	
 	//To do
 	public boolean equals(Matrix compareM){
 		return true;
+	}
+	
+    public static Matrix adjoint(Matrix A) {
+    	return (A.cofactor()).transpose();
+    }
+    
+    public static Matrix scalarMultiply(Matrix A, double scalar) {
+    	Matrix result = new Matrix(A.getNumRows(),A.getNumColumns(),true,0);
+    	for (int i=0; i<A.getNumRows(); i++) {
+    		for (int j=0; j<A.getNumColumns(); j++) {
+    			result.setElem(i, j, (scalar*A.getElem(i, j)));
+    		}
+    	}
+    	return result;
+    }
+	
+	public static boolean multiply(Matrix matrixA, Matrix matrixB, Matrix matrixResult) {
+		if((matrixA.getNumColumns() != matrixB.getNumRows()) ||
+				(matrixResult.getNumRows() != matrixA.getNumRows()) ||
+				(matrixResult.getNumColumns() != matrixB.getNumColumns()))
+			return false;
+		for(int i=0; i<matrixA.getNumRows(); i++) {
+			for(int j=0; j<matrixB.getNumColumns(); j++) {
+			    double value = 0;
+			    for(int inner=0; inner < matrixA.getNumColumns();inner++) {
+				    value = value + (matrixA.getElem(i, inner) * matrixB.getElem(inner, j));
+				}
+				matrixResult.setElem(i, j, value);
+			}
+		}
+		return true;
+	}
+	   //To do - this is copied from an example, we want to do our own
+	   // but I needed a determinant function
+	   //http://www.codeproject.com/Articles/405128/Matrix-operations-in-Java
+	public static double determinant(Matrix A) throws Exception {
+		//public static double determinant(Matrix matrix) throws NoSquareException {
+        if (!A.isSquare()) {
+        	throw new Exception("Need square matrix."); //todo define matrix exception
+        }
+		if (A.getNumRows() == 1) {
+			return A.getElem(0,0);
+		}
+		if (A.getNumRows()==2) {
+		    return (A.getElem(0, 0) * A.getElem(1, 1)) - ( A.getElem(0, 1) * A.getElem(1, 0));
+		}
+		double sum = 0.0;
+		for (int i=0; i<A.getNumColumns(); i++) {
+		    sum += flipOdd(i) * A.getElem(0, i) * determinant(Matrix.createSubMatrix(A, 0, i));
+		}
+		return sum;
+	}
+	
+	public static Matrix createSubMatrix(Matrix a, int x, int y) {
+	    Matrix mat = new Matrix(a.getNumRows()-1, a.getNumColumns()-1,true,0);
+	    int r = -1;
+	    for (int i=0;i<a.getNumRows();i++) {
+	        if (i==x)
+	            continue;
+	            r++;
+	            int c = -1;
+	        for (int j=0;j<a.getNumColumns();j++) {
+	            if (j==y)
+	                continue;
+	            mat.setElem(r, ++c, a.getElem(i, j));
+	        }
+	    }
+	    return mat;
+	}
+	
+	public Matrix cofactor() {
+		Matrix subMat = new Matrix((getNumRows()-1),(getNumColumns()-1),true,0);
+		Matrix cofactorResult = new Matrix(getNumRows(),getNumColumns(),true,0);
+		for(int i=0; i<getNumRows(); i++) {
+			for(int j=0; j<getNumColumns(); j++) {
+				subMat = createSubMatrix(this,i,j);
+				try {
+				   cofactorResult.setElem(i, j, (flipOdd(i+j)*determinant(subMat)));
+				} catch(Exception e) {
+					System.out.println(e.toString());
+				}
+			}
+		}
+        return cofactorResult;
+	}
+
+	public Matrix invert() {
+		Matrix inverse = new Matrix(getNumRows(), getNumColumns(), true, 0);
+		double det = 0;
+		try {
+		    det = determinant(this);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		det = 1/det;
+		Matrix adjointR = new Matrix(getNumRows(), getNumColumns(), true, 0);
+		adjointR = adjoint(this);
+		inverse = scalarMultiply(adjointR,det);
+		return inverse;
+	}
+	
+	public Matrix transpose() {
+		Matrix trans = new Matrix(getNumColumns(),getNumRows(),true,0);
+		for(int i=0; i<getNumRows(); i++) {
+			for(int j=0; j<getNumColumns(); j++) {
+				trans.setElem(i, j, this.getElem(j, i));
+			}
+		}
+		return trans;
+	}
+	
+	public static boolean solveLinearSystem(Matrix A, Matrix X, Matrix B) {
+		// AX=B, so A^-1B = X...Take the inverse of A and solve for X
+		Matrix invA = A.invert();
+		if(multiply(invA,B,X)) {
+			System.out.println("The matrix result is: " + X.toString());
+		} else {
+			System.out.println("Problem solving linear equation");
+		}
+		return true;
+	}
+	
+	public boolean isSquare() {
+		return (getNumRows() == getNumColumns());
 	}
 		
 	public static boolean add(Matrix A, Matrix B, Matrix C) {
@@ -83,7 +207,37 @@ public class Matrix {
 		}
 	}
 	
+	public static int flipOdd(int i) {
+		if((i % 2) == 0) return 1;
+		else 
+			return -1;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<getNumRows(); i++) {
+			for(int j=0; j<getNumColumns(); j++) {
+				sb.append(matrix[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		double[] arow = {1,1,1};
+		double[] brow = {0,2,5};
+		double[] crow = {2,5,-1};
+		Matrix linearA = new Matrix(3,3,true,0);
+		linearA.setRow(arow, 0);	
+		linearA.setRow(brow, 1);
+		linearA.setRow(crow, 2);
+		Matrix linearB = new Matrix(3,1,true,0);
+		linearB.setElem(0, 0, 6);
+		linearB.setElem(1, 0, -4);
+		linearB.setElem(2, 0, 27);
+		Matrix linResult = new Matrix(3,1,true,0);
+		solveLinearSystem(linearA,linResult,linearB);
+		System.out.println("The linear result is:\n" + linResult.toString());
 	}
 }
--- parallel-matrix/src/test/java/org/matrix/common/TestMatrixAdd.java
+++ parallel-matrix/src/test/java/org/matrix/common/TestMatrixAdd.java
@@ -9,9 +9,16 @@ import org.matrix.common.MatrixAdd1;
 
 public class TestMatrixAdd {
 
+
+	Matrix testMatrix;
+	Matrix testMatrix2;
+	Matrix testMatrix3;
 	
 	@Before
 	public void setUp() throws Exception {
+		testMatrix = new Matrix(4,4,false,10);
+		testMatrix2 = new Matrix(4,4,false,5);
+		testMatrix3 = new Matrix(3,4,false,5);
 	}
 
 	@Test
@@ -39,5 +46,94 @@ public class TestMatrixAdd {
 		if(!ret){
 			fail("Matrix add returned false");
 		}
+	}
+	
+	@Test
+	public void testGetRow() {
+		double[] rowSample = {0,2,4,6};
+		double[] temp = testMatrix.getRow(2);
+		for(int i=0; i<temp.length; i++) {
+			assert(temp[i] == rowSample[i]);
+		}
+	}
+	@Test
+	public void testGetColumn() {
+		double[] colSample = {0,2,4,6};
+		double[] temp = testMatrix.getRow(2);
+		for(int i=0; i<temp.length; i++) {
+			assert(temp[i] == colSample[i]);
+		}
+	}
+
+	
+	@Test
+	public void testMultiply() {
+		Matrix matrixResult = new Matrix(4,4,true,0);
+		Matrix.multiply(testMatrix, testMatrix2, matrixResult);
+		String expected = "0.0 0.0 0.0 0.0 \n0.0 14.0 13.0 17.0 \n0.0 28.0 26.0 34.0 \n0.0 42.0 39.0 51.0 \n";
+		assertEquals(matrixResult.toString(),expected);
+	}
+	
+	@Test
+	public void testToString() {
+		String expected = "0.0 0.0 0.0 0.0 \n0.0 1.0 2.0 3.0 \n0.0 2.0 4.0 6.0 \n0.0 3.0 6.0 9.0 \n";
+		assertEquals(testMatrix.toString(), expected);
+	}
+	
+	@Test
+	public void testSetRow() {
+		Matrix replaceRow = new Matrix(4,4,false,10);
+		double[] row = {5,5,5,5};
+		replaceRow.setRow(row, 3);
+		String expected = "0.0 0.0 0.0 0.0 \n0.0 1.0 2.0 3.0 \n0.0 2.0 4.0 6.0 \n5.0 5.0 5.0 5.0 \n";
+		assertEquals(expected,replaceRow.toString());
+		
+	}
+	@Test
+	public void testSetElem(){
+		Matrix thisMatrix = new Matrix(4,4,true,0);
+		for(int i=0;i<thisMatrix.getNumRows();i++) {
+			for(int j=0; j<thisMatrix.getNumColumns();j++) {
+				thisMatrix.setElem(i, j, ((i*j)%10));
+			}
+		}
+		String expected = "0.0 0.0 0.0 0.0 \n0.0 1.0 2.0 3.0 \n0.0 2.0 4.0 6.0 \n0.0 3.0 6.0 9.0 \n";
+		assertEquals(expected,thisMatrix.toString());
+	}
+	
+	@Test
+	public void testGetElem() {
+		assert(testMatrix.getElem(2, 2)== 4);
+	}
+	
+	@Test
+	public void testGetNumRows() {
+		Matrix newMat = new Matrix(7,3,true,0);
+		assertEquals(newMat.getNumRows(),7);
+	}
+	
+	@Test
+	public void testIsSquare() {
+		assert(testMatrix.isSquare());
+	}
+	
+	@Test 
+	public void testIsNotSquare() {
+		assert(!testMatrix3.isSquare());
+	}
+	
+	@Test
+	public void testFlipOddEven() {
+		assert(Matrix.flipOdd(2) == 1);
+	}
+	
+	public void testFlipOddOdd() {
+		assert(Matrix.flipOdd(3) == -1);
+	}
+	
+	@Test
+	public void testGetNumColumns() {
+		Matrix newMat = new Matrix(7,3,true,0);
+		assertEquals(newMat.getNumColumns(),3);
 	}
 }
